27f841ac98e07c90675fa9e49133de97d184ea2c
github5786 <aniketmethi@gmail.com>
2021-03-10 03:10:29 +0530

    hello class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import CHeader from './components/CHeader';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Start Exploring</h1>
       <Header/>
+      <CHeader/>
     </div>
   );
 }
---
+++ my-app/src/components/CHeader.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CHeader extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Class Component</h1>
+            </div>
+        )
+    }
+}
