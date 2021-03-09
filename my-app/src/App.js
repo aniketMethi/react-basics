746151cd5fcf56db0b97c4e27c27f24fe72308d8
github5786 <aniketmethi@gmail.com>
2021-03-10 03:00:18 +0530

    hello functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
    
-    <div>
+    <div className="App">
       <h1>Start Exploring</h1>
+      <Header/>
     </div>
   );
 }
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <h1>Hello Functional Component</h1>
+        </div>
+    )
+}
+
+export default Header
